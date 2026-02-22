60f63838e3a9fbde00e7c595ab1e1fc9776f2e87
swati214 <swati0019s@gmail.com>
2026-02-22 21:17:44 +0530

    Add files via upload

---
+++ lab_assign_03/-Values of Course.sql
@@ -0,0 +1,47 @@
+-Values of Course
+--CE
+INSERT INTO Course VALUES (101503, 'Geotechnical Engineering I', 4, 101, 506);
+INSERT INTO Course VALUES (101507, 'Transportation Engineering', 4, 101, 507);
+INSERT INTO Course VALUES (101505, 'Hydraulic Engineering', 4, 101, 509);
+INSERT INTO Course VALUES (101502, 'Environmental Engineering I', 4, 101, 509);
+INSERT INTO Course VALUES (101506, 'Mechanics of Materials', 4, 101, 508);
+INSERT INTO Course VALUES (101504, 'Basic Electrical and Engineering', 4, 101, 508);
+
+--ME
+INSERT INTO Course VALUES (102501, 'Fluid Machinery', 4, 102, 501);
+INSERT INTO Course VALUES (102502, 'Heat Transfer', 4, 102, 505);
+INSERT INTO Course VALUES (102503, 'Manufacturing Processes', 4, 102, 502);
+INSERT INTO Course VALUES (102504, 'Kinematics of Machinery', 4, 102, 503);
+
+--CSE
+INSERT INTO Course VALUES (105501, 'Artificial Intelligence', 4, 105, 518);
+INSERT INTO Course VALUES (105502, 'Database Management Systems', 4, 105, 519);
+INSERT INTO Course VALUES (105503, 'Formal Language and Automata', 4, 105, 523);
+INSERT INTO Course VALUES (105504, 'Software Engineering', 4, 105, 522);
+INSERT INTO Course VALUES (100508, 'Computer Network', 2, 105, 520);
+
+--EEE
+INSERT INTO Course VALUES (100506, 'Power Electronics', 4, 110, 516);
+INSERT INTO Course VALUES (100507, 'Power Systems I', 4, 110, 515);
+INSERT INTO Course VALUES (100502, 'Control Systems', 4, 110, 514);
+INSERT INTO Course VALUES (110501, 'Analog and Digital Communication', 4, 110, 513);
+
+--3D
+INSERT INTO Course VALUES (118501, '3D Modelling', 4, 118, 526);
+INSERT INTO Course VALUES (118503, '3D Texturing', 4, 118, 526);
+INSERT INTO Course VALUES (118502, '3D Character Design', 4, 118, 526);
+INSERT INTO Course VALUES (100512, 'Design Thinking', 3, 118, 527);
+
+--CECA
+INSERT INTO Course VALUES (119501, 'Machine Learning', 4, 119, 525);
+INSERT INTO Course VALUES (119504, 'Geotechnical Engineering I', 4, 119, 506);
+INSERT INTO Course VALUES (119507, 'Transportation Engineering', 4, 119, 507);
+INSERT INTO Course VALUES (119502, 'Hydraulic Engineering', 4, 119, 509);
+INSERT INTO Course VALUES (119506, 'Environmental Engineering I', 4, 119, 509);
+INSERT INTO Course VALUES (119501, 'Machine Learning', 4, 119, 525);
+INSERT INTO Course VALUES (119505, 'Hydrology and Water Resources', 4, 119, 508);
+
+--CSE AiMl
+INSERT INTO Course VALUES (621, 'Machine Learning', 4, 157, 523);
+INSERT INTO Course VALUES (622, 'Compiler Design', 4, 157, 523);
+INSERT INTO Course VALUES (623, 'Artificial Intelligence', 4, 157, 523);

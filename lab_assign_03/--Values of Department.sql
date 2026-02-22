d597ef149110683e9660f36390b0b77c07550f1a
swati214 <swati0019s@gmail.com>
2026-02-22 23:28:01 +0530

    Delete lab_assign_03/--Values of Department.sql

--- lab_assign_03/--Values of Department.sql
+++
@@ -1,8 +0,0 @@
---Values of Department 
-INSERT INTO Department VALUES (110,'Electrical and Electronic Engineering', 'Block A');
-INSERT INTO Department VALUES (102, 'Civil Engineering', 'Block B');
-INSERT INTO Department VALUES (157, 'Computer Science and Engineering (AIML) 'Block C');
-INSERT INTO Department VALUES (101, 'Mechanical Engineering ',Block D');
-INSERT INTO Department VALUES (118, '3D Animation and Graphics', 'Block E');
-INSERT INTO Department VALUES (119, 'Civil Engineering with Computer Application', 'Block F');
-INSERT INTO Department VALUES (105, 'Computer Science and Engineering', 'Block G');
